eb43ac38c28be532f8e48ef2b055f9ddd5b3ec0e
sujanthapa01 <sujanthapa4001@gmail.com>
2025-03-27 22:11:34 +0530

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Jokes from '@/components/jokes'
+function page() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Jokes />
+
+    </div>
+  )
+}
+
+export default page
